c758117b9a31cf1042e37458f4020964b376ee41
Phuoc Nguyen <mieuhamchoi@gmail.com>
2023-06-11 17:39:30 +0700

    add store with pinia, call api with axios

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,21 @@
 import { resolve } from "path";
 
 export default defineNuxtConfig({
+  // modules dependencies
+  modules: [
+    '@pinia/nuxt'
+  ],
+  // plugins dependencies
+  plugins: [
+    '~/plugins/axios/index.ts'
+  ],
   // alias config
   alias: {
       "@components": resolve(__dirname, './src/components'),
       "@pages": resolve(__dirname, './src/pages'),  
       "@assets": resolve(__dirname, './public/assets'),
+      "@stores": resolve(__dirname, './src/stores'),
+      "@plugins": resolve(__dirname, './src/plugins'),
   },
   // active dev toos
   devtools: { enabled: false },
